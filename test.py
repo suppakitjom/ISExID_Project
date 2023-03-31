8d0271e58b8eb8335458c4349f0b86be4dd826da
suppakitjom <99940355+suppakitjom@users.noreply.github.com>
2023-03-31 10:49:32 +0700

    first commit

---
+++ test.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+root = tk.Tk()
+
+tk.Button(root, text='A', background="yellow").pack()
+tk.Button(root, text='B', background="yellow").pack()
+tk.Button(root, text='C', background="yellow").pack()
+tk.Button(root, text='D', background="yellow").pack()
+
+root.mainloop()
